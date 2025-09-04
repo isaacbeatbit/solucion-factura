a1cc0dfd700c86968386647d8ebc56d141ca9d53
Isaac Lop <beatbit01@gmail.com>
2025-09-04 14:11:24 -0600

    partners added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    allowedHosts: [
-      "rnhdb-2806-106e-15-3497-d9ca-c403-76e-8e77.a.free.pinggy.link",
-    ],
-  },
+  integrations: [sitemap()],
+  site: "http://solucionfactura.mx",
 });
