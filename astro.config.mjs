30ef7361271551654de8736f89aea02875fd7fd2
Isaac Lopez <beatbit01@gmail.com>
2025-11-19 21:22:43 -0600

    all ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
   site: "http://solucionfactura.mx",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
